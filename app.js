abb8adbd67b5c8f9d642701c548a8b9ef0761a0a
amirpoint <onceler51@gmail.com>
2022-12-17 20:52:16 +0330

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}...`);
+});
