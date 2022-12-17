90c9388111710c3886a3cbceead23288608fe488
amirpoint <onceler51@gmail.com>
2022-12-17 21:35:20 +0330

    Add register and login requests

--- app.js
+++ app.js
@@ -14,6 +14,69 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+//mongoose
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+  
+}
+
+const userSchema = {
+    email: String,
+    pw: String
+};
+
+const User = new mongoose.model('User', userSchema);
+
+
+//get requests
+app.get('/', (req, res) => {
+    res.render('home');
+    
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+    
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+
+});
+
+
+//post requests
+app.post('/register', (req, res) =>{
+    const newUser = new User({
+        email: req.body.username,
+        pw: req.body.password
+    });
+    newUser.save((err) => {
+        if (!err) {
+            console.log('New user added.');
+            res.render('secrets');
+        }
+    });
+
+});
+
+app.post('/login', async (req, res) => {
+    const email = req.body.username;
+    const pw = req.body.password;
+
+    const userMatches = await User.find({email: email});
+    if (userMatches) {
+        if (userMatches[0].pw=== pw) res.render('secrets');
+        res.send('Password is incorrect..!');
+
+    } else {
+        res.send('User not found..!');
+    }
+
+});
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}...`);
