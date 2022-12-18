2e724784cd39aeaa0cc8a6f7093ec8a1a273e28b
amirpoint <onceler51@gmail.com>
2022-12-19 00:10:34 +0330

    Restoring data with passport and add google Oauth

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
-const port = 3000;
+const port = '3000';
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -29,16 +31,36 @@ async function main() {
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+  
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    User.findOrCreate({ googleId: profile.id }, (err, user) => {
+      return cb(err, user);
+    });
+  }
+));
 
 
 //get requests
@@ -56,6 +78,18 @@ app.get('/register', (req, res) => {
     res.render('register');
 
 });
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/secrets', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // successful authentication, redirect to secrets
+        res.redirect('/secrets');
+    }
+);
 
 app.get('/secrets', (req, res) => {
     if (req.isAuthenticated()) {
